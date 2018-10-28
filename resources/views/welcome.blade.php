3749bf8d0cde415867df61d0bb7a32e97be752e3
3A532066 <bcdling05@gmail.com>
2018-10-29 00:33:28 +0800

    練習7-2HomeController內容

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                <a href="{{ route('hello.index',['name'=>'Tom'])}}">Hello</a>
                 </div>
 
                 <div class="links">
