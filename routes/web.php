c876ed63386ca392b6f0ff44b0967e5ef4393357
3A532066 <bcdling05@gmail.com>
2018-10-29 00:18:37 +0800

    練習7-1編輯app/Http/routes.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
+//Route::get('/', ['as'=>'home.index', 'uses'=>'HomeController@index']);
+Route::get('/', 'HomeController@index')->name('home.index');
+
 Route::get('say/{name?}',['as' => 'hello.index', function ($name = 'Everybody') {
     return 'Hello, '.$name;
 }]);
